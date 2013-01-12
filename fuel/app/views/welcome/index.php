5ab62eac328f6a687081160e4e71c0fcdd9bc0ae
foostan <foostan@git.localdomain>
2013-01-12 10:03:05 +0900

    jQuery Mobileの導入

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -3,48 +3,59 @@
 <head>
 	<meta charset="utf-8">
 	<title>FuelPHP Framework</title>
-	<?php echo Asset::css('bootstrap.css'); ?>
-	<?php echo Asset::css('bootstrap-responsive.css'); ?>
+	<?php echo Asset::css('themes/default/jquery.mobile-1.2.0.css'); ?>
+	<?php echo Asset::js('jquery.js'); ?>
+	<?php echo Asset::js('jquery.mobile-1.2.0.js'); ?>
 </head>
-<body>
-	<div class="container">
-		<div class="row">
-			<div class="span12">
-				<h1>Welcome</h1>
-				<p>You have successfully installed the FuelPHP Framework.</p>
-				<p><a class="btn btn-primary btn-large" href="http://docs.fuelphp.com">Read the Docs</a></p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="span4">
-				<h2>Get Started</h2>
-				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
-				<p>This view can be found at <code>APPPATH/views/welcome/index.php</code>.</p>
-				<p>You can modify these files to get your application started quickly.</p>
-			</div>
-			<div class="span4">
-				<h2>Learn</h2>
-				<p>The best way to learn FuelPHP is reading through the <a href="http://docs.fuelphp.com">Documentation</a>.</p>
-				<p>Another good resource is the <a href="http://fuelphp.com/forums">Forums</a>.  They are fairly active, and you can usually get a response quickly.</p>
-			</div>
-			<div class="span4">
-				<h2>Contribute</h2>
-				<p>FuelPHP wouldn't exist without awesome contributions from the community.  Use the links below to get contributing.</p>
-				<ul>
-					<li><a href="http://docs.fuelphp.com/general/coding_standards.html">Coding Standards</a></li>
-					<li><a href="http://github.com/fuel/fuel">GitHub Respository</a></li>
-					<li><a href="http://fuelphp.com/contribute/issue-tracker">Issue Tracker</a></li>
+<div data-role="page" class="type-home">
+	<div data-role="content">
+		<div class="content-secondary">
+			<p class="intro"><strong>Welcome.</strong> jQuery Mobile is the easiest way to build sites and apps that are accessible on all popular smartphone, tablet and desktop devices. For jQuery 1.7.0 to 1.8.2.</p>
+
+			<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="f">
+				<li data-role="list-divider">Overview</li>
+				<li><a href="docs/about/intro.html">Intro to jQuery Mobile</a></li>
+				<li><a href="docs/about/getting-started.html">Quick start guide</a></li>
+				<li><a href="docs/about/features.html">Features</a></li>
+				<li><a href="docs/about/accessibility.html">Accessibility</a></li>
+				<li><a href="docs/about/platforms.html">Supported platforms</a></li>
+			</ul>
+
+		</div><!--/content-primary-->
+
+		<div class="content-primary">
+			<nav>
+
+
+				<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
+					<li data-role="list-divider">Components</li>
+					<li><a href="docs/pages/index.html">Pages &amp; dialogs</a></li>
+					<li><a href="docs/toolbars/index.html">Toolbars</a></li>
+					<li><a href="docs/buttons/index.html">Buttons</a></li>
+					<li><a href="docs/content/index.html">Content formatting</a></li>
+					<li><a href="docs/forms/index.html">Form elements</a></li>
+					<li><a href="docs/lists/index.html">Listviews</a></li>
+
+					<li data-role="list-divider">API</li>
+					<li><a href="docs/api/globalconfig.html">Configuring defaults</a></li>
+					<li><a href="docs/api/events.html">Events</a></li>
+					<li><a href="docs/api/methods.html">Methods &amp; Utilities</a></li>
+					<li><a href="docs/api/data-attributes.html">Data attribute reference</a></li>
+					<li><a href="docs/api/themes.html">Theme framework</a></li>
+
+
 				</ul>
-			</div>
+			</nav>
 		</div>
-		<hr/>
-		<footer>
-			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
-			<p>
-				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
-				<small>Version: <?php echo Fuel::VERSION; ?></small>
-			</p>
-		</footer>
+
+
+
 	</div>
-</body>
+
+	<div data-role="footer" class="footer-docs" data-theme="c">
+            <p class="jqm-version"></p>
+			<p>&copy; 2012 jQuery Foundation and other contributors</p>
+	</div>
+
+</div>
 </html>
